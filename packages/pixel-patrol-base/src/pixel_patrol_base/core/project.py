d12acd6bb96abe8d94957c62c0c066e071c02474
bellonet <bellonet@gmail.com>
2025-09-10 18:49:13 +0200

    warning for no loader

--- packages/pixel-patrol-base/src/pixel_patrol_base/core/project.py
+++ packages/pixel-patrol-base/src/pixel_patrol_base/core/project.py
@@ -28,6 +28,8 @@ class Project:
         self.settings: Settings = Settings()
         self.images_df: Optional[pl.DataFrame] = None
 
+        if loader is None:
+            logger.warning(f"Project Core: No loader specified for project '{self.name}'. Only basic file information will be extracted.")
         logger.info(f"Project Core: Project '{self.name}' initialized with loader {self.loader.NAME if self.loader else "None" } and base dir: {self.base_dir}.")
 
 
