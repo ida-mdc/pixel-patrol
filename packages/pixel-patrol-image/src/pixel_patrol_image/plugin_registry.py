81db61c1f341c54010b240f26386de3f213787c4
bellonet <bellonet@gmail.com>
2025-09-16 15:13:25 +0200

    fix wrong import path

--- packages/pixel-patrol-image/src/pixel_patrol_image/plugin_registry.py
+++ packages/pixel-patrol-image/src/pixel_patrol_image/plugin_registry.py
@@ -1,5 +1,6 @@
 from pixel_patrol_image.plugins.processors.quality_metrics_processor import QualityMetricsProcessor
-from pixel_patrol_image.plugins.widgets.image_quality import ImageQualityWidget, DynamicQualityMetricsWidget
+from pixel_patrol_image.plugins.widgets.image_quality import ImageQualityWidget
+from pixel_patrol_image.plugins.widgets.dynamic_quality_metrics import DynamicQualityMetricsWidget
 
 def register_processor_plugins():
     return [
