8b468bb9d895303ba28a4fbd42266986569d16cf
Thomas Roßberg <thomas.rossberg@mdc-berlin.de>
2026-04-24 17:02:02 +0200

    add nan count to basic statistics to get them also slice wise

--- packages/pixel-patrol-base/src/pixel_patrol_base/plugins/processors/basic_stats_processor.py
+++ packages/pixel-patrol-base/src/pixel_patrol_base/plugins/processors/basic_stats_processor.py
@@ -4,6 +4,7 @@ from typing import Dict, Callable
 import numpy as np
 
 from pixel_patrol_base.core.specs import RecordSpec
+from pixel_patrol_base.core.record import Record
 from pixel_patrol_base.utils.array_utils import calculate_np_array_stats
 from pixel_patrol_base.core.feature_schema import validate_processor_output
 
@@ -15,6 +16,8 @@ def _column_fn_registry() -> Dict[str, Dict[str, Callable]]:
         'std_intensity': {'fn': lambda a: np.float32(np.nanstd(a)) if a.size else np.float32(np.nan), 'agg': np.nanmean},
         'min_intensity': {'fn': lambda a: np.float32(np.nanmin(a)) if a.size else np.float32(np.nan), 'agg': np.nanmin},
         'max_intensity': {'fn': lambda a: np.float32(np.nanmax(a)) if a.size else np.float32(np.nan), 'agg': np.nanmax},
+        'nan_count': {'fn': lambda a: np.float32(np.sum(np.isnan(a))) if a.size else np.float32(np.nan),
+                      'agg': np.nansum},
     }
 
 class BasicStatsProcessor:
@@ -27,7 +30,7 @@ class BasicStatsProcessor:
 
     DESCRIPTION = "Extracts basic image statistics such as mean, min, max."
 
-    def run(self, art):
+    def run(self, art: Record):
         dim_order = art.dim_order
         registry = _column_fn_registry()
         result = calculate_np_array_stats(art.data, dim_order, registry)
