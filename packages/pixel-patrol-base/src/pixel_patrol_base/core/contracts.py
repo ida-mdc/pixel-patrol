f67b6b67e42257c9961d9dd08db8cdc21bb2029b
oceanites <15113257+oceanites@users.noreply.github.com>
2026-07-10 12:54:15 +0200

    Update contracts.py
    add note that X and Y are somewhat special

--- packages/pixel-patrol-base/src/pixel_patrol_base/core/contracts.py
+++ packages/pixel-patrol-base/src/pixel_patrol_base/core/contracts.py
@@ -34,7 +34,7 @@ class FileInfo:
     """
     shape:     Tuple[int, ...]
     dtype:     Any                # numpy dtype or compatible
-    dim_order: str                # e.g. 'ZYX'
+    dim_order: str                # e.g. 'ZYX', specifying 'X' and 'Y' is important for many processors
     n_images:  int = 1            # >1 for container formats (LMDB, multi-series OME-TIFF, …)
 
 
