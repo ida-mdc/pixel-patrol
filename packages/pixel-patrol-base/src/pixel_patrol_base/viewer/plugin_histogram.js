b8abf5893f4bbe25915f15cda6bab9611121ea72
oceanites <15113257+oceanites@users.noreply.github.com>
2026-09-21 14:45:48 +0200

    fix bug in histogram viewers 'select groups' functionality
    using the raw group column results in a day-granularity -> datetime cast, so that a day '2020-01-01' will result in '2020-01-01 00:00:00'. checking against this will result in an (almost everytime) empty histogram.

--- packages/pixel-patrol-base/src/pixel_patrol_base/viewer/plugin_histogram.js
+++ packages/pixel-patrol-base/src/pixel_patrol_base/viewer/plugin_histogram.js
@@ -428,6 +428,7 @@ export default {
         if (ctrl.selectedGroups.length && ctx.state.groupCol) {
           const list = ctrl.selectedGroups.map(g => `'${g.replace(/'/g, "''")}'`).join(', ');
           const extra = `${q(ctx.state.groupCol)} IN (${list})`;
+          // NEW: const extra = `${ctx.sql.groupCol()} IN (${list})`;
           where = where ? `${where} AND ${extra}` : `WHERE ${extra}`;
         }
 
